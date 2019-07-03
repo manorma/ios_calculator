87cc3d70f5aaafc0c6c57258ab458f797d0baf43
Manorm Bharti <manorma.bharti@resideo.com>
2019-07-03 18:11:00 +0530

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Manorma Bharti on 03/07/19.
+//  Copyright © 2019 appcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
